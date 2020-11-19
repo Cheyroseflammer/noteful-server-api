502bd67b60803db3797e8d21b9c047da123876a0
Chey Rose Flammer <cheyroseflammer@gmail.com>
2020-11-18 16:43:47 -0800

    first commit of fixed noteful server

---
+++ src/folders/folders-service.js
@@ -0,0 +1,25 @@
+const FoldersService = {
+  getAllFolders(knex) {
+    return knex.select('*').from('noteful_folders');
+  },
+  insertFolder(knex, data) {
+    return knex
+      .insert(data)
+      .into('noteful_folders')
+      .returning('*')
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex.select('*').from('noteful_folders').where({ id }).first();
+  },
+  deleteFolder(knex, id) {
+    return knex('noteful_folders').where({ id }).delete();
+  },
+  updateFolder(knex, id, newFolderFields) {
+    return knex('noteful_folders').where({ id }).update(newFolderFields);
+  },
+};
+
+module.exports = FoldersService;
---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const app = require('./app');
+const knex = require('knex');
+
+const { PORT, DATABASE_URL, NODE_ENV } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
+});
+app.set('db', db);
+
+app.listen(PORT, () => {
+  console.log(`Server listening in ${NODE_ENV} mode on port ${PORT}`)
+});
